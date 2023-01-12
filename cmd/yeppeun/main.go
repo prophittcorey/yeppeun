1e280f69c40fedca2d4826f6c831eacc8d80a5ed
Corey Prophitt <git@prophitt.me>
2023-01-12 05:47:12 -0800

    Added command line options

--- cmd/yeppeun/main.go
+++ cmd/yeppeun/main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
 	"github.com/prophittcorey/yeppeun/internal/web"
 )
 
+func setenv(key, value string) {
+	if err := os.Setenv(key, value); err != nil {
+		log.Printf("yeppeun: failed to set a default environment variable; %s", err)
+	}
+}
+
 func main() {
+	var (
+		help   bool
+		port   string
+		host   string
+		domain string
+	)
+
+	flag.BoolVar(&help, "h", false, "Displays the program's usage.")
+	flag.StringVar(&port, "port", "3000", "The port to run the server on (default: 3000).")
+	flag.StringVar(&host, "host", "127.0.0.1", "The host to run the server on (default: 127.0.0.1).")
+	flag.StringVar(&domain, "domain", "localhost", "The domain name for the server (default: localhost).")
+
+	flag.Parse()
+
+	if help {
+		flag.Usage()
+		return
+	}
+
+	setenv("HOST", host)
+	setenv("PORT", port)
+	setenv("DOMAIN", domain)
+
 	if err := web.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
