48a105e8ae586218f7401e75dad554f22330ad7b
Corey Prophitt <git@prophitt.me>
2023-01-19 07:56:30 -0800

    Added copied notification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,19 @@
 (function (d) {
   var cleaned = d.querySelector('code.language-js');
   var copy = d.querySelector('pre > .copy');
+  var msg = d.querySelector('pre > .copy > span');
 
   if (cleaned && copy) {
     copy.addEventListener('click', function () {
-      console.log('copied');
+      if (msg) {
+        msg.style.opacity = 1;
+
+        setTimeout(function () {
+          msg.style.opacity = 0;
+        }, 1250);
+      }
+
       navigator.clipboard.writeText(cleaned.innerText);
     });
   }
-
 })(document)
